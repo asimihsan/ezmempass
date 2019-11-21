616c3b0cf316e8d0b993b73965969c9dbab9089f
Asim Ihsan <asim.ihsan@gmail.com>
2019-11-20 21:21:31 -0800

    flutter_app - beginning UI dev

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,14 +2,9 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'dart:developer' as developer;
 
 void main() {
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.landscapeLeft,
-    DeviceOrientation.landscapeRight]).then((_) {
-      runApp(MyApp());
-    });
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,20 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'EzMemPass',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'EzMemPass'),
     );
   }
 }
@@ -55,95 +41,164 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   static const platform = const MethodChannel('com.asimihsan/generate_passphrase');
+  String _password = '';
   String _passphrase = '';
+  int _passphraseLength = 7;
 
   Future<void> _generatePassphrase() async {
+    String password;
     String passphrase;
     try {
-      final String input = '{"passphrase_length": 7}';
+      final Map<String, dynamic> inputMap = new Map();
+      inputMap["passphrase_length"] = _passphraseLength;
+      final String input = jsonEncode(inputMap);
       final String result = await platform.invokeMethod(
           'generatePassphrase', {"input": input});
       final Map<String, dynamic> resultDeser = jsonDecode(result);
-      final String generatedPassword = resultDeser['password'];
-      final String generatedPassphrase = resultDeser['passphrase'];
-
-      final StringBuffer resultBuffer = new StringBuffer();
-      resultBuffer.write("Password: ");
-      resultBuffer.write(generatedPassword);
-      resultBuffer.write("\n");
-      resultBuffer.write("Passphrase: ");
-      resultBuffer.write(generatedPassphrase);
-      passphrase = resultBuffer.toString();
+      password = resultDeser['password'];
+      passphrase = resultDeser['passphrase'];
     } on PlatformException catch (e) {
       passphrase = "Failed to get passphrase: '${e.message}'.";
     }
     setState(() {
+      _password = password;
       _passphrase = passphrase;
-    });
-  }
-
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    // ------------------------------------------------------------------------
+    //  Password
+    // ------------------------------------------------------------------------
+    final Widget passwordLabel = Text(
+      "Password",
+      style: Theme.of(context).textTheme.headline,
+    );
+    final Widget passwordValue = Expanded(
+      child: SelectableText(
+        _password,
+        style: Theme.of(context).textTheme.title,
+      )
+    );
+    final Widget passwordContainer = Container(
+        padding: const EdgeInsets.all(32),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Passphrase:',
+            Row(
+              children: <Widget>[
+                passwordLabel,
+              ],
             ),
-            Text(
-              '$_passphrase',
-              style: Theme.of(context).textTheme.body1,
+            Row(
+              children: <Widget>[
+                passwordValue
+              ],
+            )
+          ],
+        )
+    );
+    // ------------------------------------------------------------------------
+
+    // ------------------------------------------------------------------------
+    //  Passphrase
+    // ------------------------------------------------------------------------
+    final Widget passphraseLabel = Text(
+      "Passphrase",
+      style: Theme.of(context).textTheme.headline,
+    );
+    final Widget passphraseValue = Expanded(
+      child: SelectableText(
+        _passphrase,
+        style: Theme.of(context).textTheme.title,
+      )
+    );
+    final Widget passphraseContainer = Container(
+        padding: const EdgeInsets.fromLTRB(32, 0, 32, 32),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                passphraseLabel,
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                passphraseValue
+              ],
+            )
+          ],
+        )
+    );
+    // ------------------------------------------------------------------------
+
+    // ------------------------------------------------------------------------
+    //  Number of words slider.
+    // ------------------------------------------------------------------------
+    final Widget numberOfWordsLabel = Text(
+      "Number of words",
+      style: Theme.of(context).textTheme.headline,
+    );
+    final double minPassphraseLength = 4.0;
+    final double maxPassphraseLength = 25.0;
+    final int divisions = (maxPassphraseLength - minPassphraseLength).round() + 1;
+    final Widget numberOfWordsSlider = Flexible(
+      flex: 1,
+      child: Slider(
+        min: 4.0,
+        max: 25.0,
+        value: _passphraseLength.toDouble(),
+        divisions: divisions,
+        onChanged: (newPassphraseLength) {
+          setState(() {
+            _passphraseLength = newPassphraseLength.toInt();
+          });
+        },
+      )
+    );
+    final Widget numberOfWordsSliderValue = Text(
+      '$_passphraseLength',
+      style: Theme.of(context).textTheme.title,
+    );
+    final Widget numberOfWordsContainer = Container(
+        padding: const EdgeInsets.fromLTRB(32, 0, 32, 32),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                numberOfWordsLabel
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                numberOfWordsSlider,
+                numberOfWordsSliderValue
+              ],
             ),
           ],
-        ),
+        )
+    );
+    // --------------
+    // ------------------------------------------------------------------------
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            passwordContainer,
+            passphraseContainer,
+            numberOfWordsContainer,
+          ],
+        )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _generatePassphrase,
         tooltip: 'Generate',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }

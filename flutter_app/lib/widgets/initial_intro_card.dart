d1da1610c69ad57eec9063c527baafdad6188d92
Jack Vaught <johnmvaught@gmail.com>
2020-01-19 15:53:08 -0800

    fixes landscape padding, adds safeArea, creates intro-card widget

---
+++ flutter_app/lib/widgets/initial_intro_card.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class InitialIntroCard extends StatelessWidget {
+  final String cardText;
+
+  @override
+
+  InitialIntroCard({
+    @required this.cardText,
+  });
+
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(40),
+      margin: EdgeInsets.only(
+        top: 50.0,
+        left: 40.0,
+        right: 40.0,
+        bottom: 10.0,
+      ),
+      decoration: BoxDecoration(color: Theme.of(context).canvasColor),
+      child: Align(
+          alignment: Alignment.center,
+          child: Text(
+            cardText,
+            textAlign: TextAlign.center,
+            style: Theme.of(context)
+                .textTheme
+                .headline
+                .copyWith(fontFamily: 'RecursiveSansLinear'),
+          )),
+    );
+  }
+}

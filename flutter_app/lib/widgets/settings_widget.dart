0e9ac61803cea0a454db38ac56315e214320d7b2
Asim Ihsan <asim.ihsan@gmail.com>
2020-04-26 13:29:46 -0700

    flutter - settings, app icons

---
+++ flutter_app/lib/widgets/settings_widget.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_app/models/preferences_model.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:provider/provider.dart';
+
+class SettingsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Icon backIcon;
+    if (Platform.isIOS) {
+      backIcon = new Icon(CupertinoIcons.back);
+    } else {
+      backIcon = new Icon(Icons.arrow_back);
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("Settings"),
+            leading: new IconButton(
+              icon: backIcon,
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )),
+        body: SettingsListView());
+  }
+}
+
+const ABOUT_TEXT = """## About EzMemPass
+
+**Version**: 1.0 build 4
+**Authors**: Asim Ihsan and Jack Vaught
+
+""";
+
+class SettingsListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Container(
+      padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+      child: ListView(
+        physics: ClampingScrollPhysics(),
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 32.0),
+            child: RaisedButton(
+              child: Text("Reset settings"),
+              onPressed: () async {
+                final PreferencesModel preferencesModel =
+                    Provider.of<PreferencesModel>(context, listen: false);
+                await preferencesModel.clear();
+                Scaffold.of(context)
+                    .showSnackBar(SnackBar(content: Text("Successfully reset settings.")));
+              },
+            ),
+          ),
+          Card(
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+              child: MarkdownBody(
+                data: ABOUT_TEXT,
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}

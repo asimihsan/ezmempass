a466c786c84752a98fb11201e1bbcf20b54d12e3
Asim Ihsan <asim.ihsan@gmail.com>
2020-04-26 22:14:45 -0700

    cdk - create simple support URL too

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,9 +1,26 @@
 #!/usr/bin/env node
 import * as cdk from '@aws-cdk/core';
-import { CdkStack } from '../lib/cdk-stack';
+import { StaticSiteCdkStack } from '../lib/static-site-cdk-stack';
 
 const app = new cdk.App();
 const environment = { account: '519160639284', region: 'us-west-2' }
-const domainName = 'ezmempass-privacy-policy.ihsan.io'
 
-new CdkStack(app, 'prod-EzMemPassPrivacyPolicyStack', domainName, { env: environment });
+const privacyPolicyDomainName = 'ezmempass-privacy-policy.ihsan.io'
+const privacyPolicySourceFolder = '../docs/privacy_policy/'
+new StaticSiteCdkStack(
+    app,
+    'prod-EzMemPassPrivacyPolicyStack',
+    privacyPolicyDomainName,
+    privacyPolicySourceFolder,
+    { env: environment }
+);
+
+const supportSiteDomainName = 'ezmempass.ihsan.io'
+const supportSiteSourceFolder = '../docs/support_site/'
+new StaticSiteCdkStack(
+    app,
+    'prod-EzMemPassSupportSiteStack',
+    supportSiteDomainName,
+    supportSiteSourceFolder,
+    { env: environment }
+);

b8cfe42447905f4e6670b0fff938840887e03604
Asim Ihsan <ssh@sshemail.ihsan.io>
2023-01-12 16:51:17 -0800

    update site with app links

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
-import { StaticSiteCdkStack } from '../lib/static-site-cdk-stack';
+
+import * as cdk from 'aws-cdk-lib';
+import {StaticSiteCdkStack} from '../lib/static-site-cdk-stack';
 
 const app = new cdk.App();
-const environment = { account: '519160639284', region: 'us-west-2' }
+const environment = {account: '519160639284', region: 'us-west-2'}
 
 const privacyPolicyDomainName = 'ezmempass-privacy-policy.ihsan.io'
 const privacyPolicySourceFolder = '../docs/privacy_policy/'
@@ -12,15 +13,15 @@ new StaticSiteCdkStack(
     'prod-EzMemPassPrivacyPolicyStack',
     privacyPolicyDomainName,
     privacyPolicySourceFolder,
-    { env: environment }
+    {env: environment}
 );
 
 const supportSiteDomainName = 'ezmempass.ihsan.io'
 const supportSiteSourceFolder = '../docs/support_site/'
 new StaticSiteCdkStack(
     app,
-    'prod-EzMemPassSupportSiteStack',
+    'prod-EzMemPassSupportSiteStack2',
     supportSiteDomainName,
     supportSiteSourceFolder,
-    { env: environment }
+    {env: environment}
 );
--- cdk/lib/static-site-cdk-stack.ts
+++ cdk/lib/static-site-cdk-stack.ts
@@ -1,83 +1,82 @@
-import * as cdk from '@aws-cdk/core';
-
-import * as certificatemanager from '@aws-cdk/aws-certificatemanager';
-import * as cloudfront from '@aws-cdk/aws-cloudfront';
-import * as route53 from '@aws-cdk/aws-route53';
-import * as s3 from '@aws-cdk/aws-s3';
-import * as s3deployment from '@aws-cdk/aws-s3-deployment';
-import * as targets from '@aws-cdk/aws-route53-targets';
+import * as cdk from 'aws-cdk-lib';
+import {
+    aws_certificatemanager as certificatemanager,
+    aws_cloudfront as cloudfront,
+    aws_cloudfront_origins as origins,
+    aws_route53 as route53,
+    aws_route53_targets as route53_targets,
+    aws_s3 as s3,
+    aws_s3_deployment as s3_deployment
+} from 'aws-cdk-lib';
 
 export class StaticSiteCdkStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, domainName: string, sourceFolder: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.App, id: string, domainName: string, sourceFolder: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // ------------------------------------------------------------------------
-    //  Public S3 bucket containing the static site.
-    // ------------------------------------------------------------------------
-    const siteBucket = new s3.Bucket(this, 'SiteBucket', {
-      bucketName: domainName,
-      websiteIndexDocument: 'index.html',
-      websiteErrorDocument: 'error.html',
-      publicReadAccess: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-    });
-    // ------------------------------------------------------------------------
+        // ------------------------------------------------------------------------
+        //  Public S3 bucket containing the static site.
+        // ------------------------------------------------------------------------
+        const siteBucket = new s3.Bucket(this, 'SiteBucket', {
+            bucketName: domainName,
+            websiteIndexDocument: 'index.html',
+            websiteErrorDocument: 'error.html',
+            publicReadAccess: true,
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+        });
+        // ------------------------------------------------------------------------
 
-    // ------------------------------------------------------------------------
-    //  Certificate.
-    // ------------------------------------------------------------------------
-    const hostedZone = route53.HostedZone.fromLookup(this, 'HostedZone', {
-      domainName: 'ihsan.io',
-      privateZone: false
-    });
-    const certificate = new certificatemanager.DnsValidatedCertificate(this, "Certificate", {
-      domainName: domainName,
-      region: "us-east-1",
-      hostedZone: hostedZone
-    });
-    // ------------------------------------------------------------------------
+        // ------------------------------------------------------------------------
+        //  Certificate.
+        // ------------------------------------------------------------------------
+        const hostedZone = route53.HostedZone.fromLookup(this, 'HostedZone', {
+            domainName: 'ihsan.io',
+            privateZone: false
+        });
+        const certificate = new certificatemanager.DnsValidatedCertificate(this, "Certificate", {
+            domainName: domainName,
+            region: "us-east-1",
+            hostedZone: hostedZone
+        });
+        // ------------------------------------------------------------------------
 
-    // ------------------------------------------------------------------------
-    //  CloudFront distribution in front of the S3 bucket.
-    // ------------------------------------------------------------------------
-    const distribution = new cloudfront.CloudFrontWebDistribution(this, 'SiteDistribution', {
-      viewerCertificate: cloudfront.ViewerCertificate.fromAcmCertificate(certificate, {
-        aliases: [domainName],
-        securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2018,
-        sslMethod: cloudfront.SSLMethod.SNI,
-      }),
-      originConfigs: [
-        {
-          s3OriginSource: {
-            s3BucketSource: siteBucket
-          },
-          behaviors: [{ isDefaultBehavior: true, compress: true }],
-        }
-      ],
-      priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
-      viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
-    });
-    // ------------------------------------------------------------------------
+        // ------------------------------------------------------------------------
+        //  CloudFront distribution in front of the S3 bucket.
+        // ------------------------------------------------------------------------
+        const distribution = new cloudfront.Distribution(this, 'SiteDistribution', {
+            defaultBehavior: {
+                origin: new origins.S3Origin(siteBucket),
+                viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
+            },
+            certificate: certificate,
+            domainNames: [domainName],
+            priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
+        });
 
-    // ------------------------------------------------------------------------
-    //  Route 53 alias record pointing at the CloudFront distribution.
-    // ------------------------------------------------------------------------
-    new route53.ARecord(this, 'SiteAliasRecord', {
-      zone: hostedZone,
-      recordName: domainName + ".",
-      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution))
-    });
-    // ------------------------------------------------------------------------
+        // Upgrade! https://docs.aws.amazon.com/cdk/api/v1/docs/aws-cloudfront-readme.html#migrating-from-the-original-cloudfrontwebdistribution-to-the-newer-distribution-construct
+        const cfnDistribution = distribution.node.defaultChild as cloudfront.CfnDistribution;
+        cfnDistribution.overrideLogicalId('SiteDistributionCFDistribution209CF7F5');
+        // ------------------------------------------------------------------------
 
-    // ------------------------------------------------------------------------
-    //  Actually deploy the site
-    // ------------------------------------------------------------------------
-    new s3deployment.BucketDeployment(this, 'DeployWithInvalidation', {
-      sources: [s3deployment.Source.asset(sourceFolder)],
-      destinationBucket: siteBucket,
-      distribution,
-      distributionPaths: ['/*'],
-    });
-    // ------------------------------------------------------------------------
-  }
+        // ------------------------------------------------------------------------
+        //  Route 53 alias record pointing at the CloudFront distribution.
+        // ------------------------------------------------------------------------
+        new route53.ARecord(this, 'SiteAliasRecord', {
+            zone: hostedZone,
+            recordName: domainName + ".",
+            target: route53.RecordTarget.fromAlias(new route53_targets.CloudFrontTarget(distribution))
+        });
+        // ------------------------------------------------------------------------
+
+        // ------------------------------------------------------------------------
+        //  Actually deploy the site
+        // ------------------------------------------------------------------------
+        new s3_deployment.BucketDeployment(this, 'DeployWithInvalidation', {
+            sources: [s3_deployment.Source.asset(sourceFolder)],
+            destinationBucket: siteBucket,
+            distribution,
+            distributionPaths: ['/*'],
+            memoryLimit: 1024,
+        });
+        // ------------------------------------------------------------------------
+    }
 }

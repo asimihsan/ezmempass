05a323d14ce5de47ea3fde2be5e3efba86c8980a
Asim Ihsan <ssh@sshemail.ihsan.io>
2025-05-13 08:43:48 -0400

    leptos csr example

--- apps/ezmempass-app/src/main.rs
+++ apps/ezmempass-app/src/main.rs
@@ -1,7 +1,14 @@
 use leptos::prelude::*;
+use ezmempass_app::App;
 
 fn main() {
+    // set up logging
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
-    leptos::mount::mount_to_body(|| view! { <p>"Hello, world!"</p> })
+
+    mount_to_body(|| {
+        view! {
+            <App />
+        }
+    })
 }

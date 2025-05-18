6eea834672a100b5c321b545d56164ff5ab2f2fc
Asim Ihsan <ssh@sshemail.ihsan.io>
2025-05-18 06:32:28 -0400

    wip trying to get workers to work

--- libs/ezmempass-worker/src/lib.rs
+++ libs/ezmempass-worker/src/lib.rs
@@ -1,9 +1,45 @@
 use ezmempass_core::{generator::PasswordGeneratorFactory, types::*};
-use gloo_worker::oneshot::oneshot;
+use futures::{sink::SinkExt, StreamExt};
+use gloo_worker::reactor::{reactor, ReactorScope};
 
-#[oneshot]
-async fn EzMemPassWorker(opts: GenerationOptions) -> GeneratedPassword {
-    PasswordGeneratorFactory::create(opts.preferred_method)
-        .generate(&opts)
-        .unwrap()
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub enum Request {
+    GeneratePassword(GenerationOptions),
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub enum Response {
+    GeneratedPassword(GeneratedPassword),
+}
+
+#[reactor]
+pub async fn EzMemPassWorker(mut scope: ReactorScope<Request, Response>) {
+    log::info!("EzMemPassWorkerReactor function triggered");
+    while let Some(m) = scope.next().await {
+        match m {
+            Request::GeneratePassword(opts) => {
+                log::info!(
+                    "EzMemPassWorkerReactor processing message. opts: {:?}",
+                    opts
+                );
+
+                let result = PasswordGeneratorFactory::create(opts.preferred_method)
+                    .generate(&opts)
+                    .unwrap();
+
+                log::info!("EzMemPassWorkerReactor finished processing message, sending result");
+
+                if scope
+                    .send(Response::GeneratedPassword(result))
+                    .await
+                    .is_err()
+                {
+                    log::error!("EzMemPassWorkerReactor failed to send result");
+                    break;
+                }
+
+                log::info!("EzMemPassWorkerReactor sent result");
+            }
+        }
+    }
 }
